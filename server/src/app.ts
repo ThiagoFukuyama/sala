2594adeb9e4549d2a99a2e910df9ed3f31007c20
Thiago Fukuyama Marcilli <thiagofkm@gmail.com>
2024-03-06 17:32:31 -0300

    Adiciona Helmet.js ao projeto

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import helmet from "helmet";
 import dotenv from "dotenv";
 
 import { apiRouter } from "./routes/api";
@@ -7,6 +8,8 @@ import { errorHandler, notFound } from "./middleware/errorMiddleware";
 dotenv.config();
 
 export const app = express();
+
+app.use(helmet());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
