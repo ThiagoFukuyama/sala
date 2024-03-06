be684bc14ef74407b3832253537684173de47e5c
Thiago Fukuyama Marcilli <thiagofkm@gmail.com>
2024-03-06 17:42:20 -0300

    Atualiza index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,7 @@
 import { app } from "./app";
 
-const server = app;
-
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
+const server = app.listen(PORT, () => {
    console.log(`Server listening on port ${PORT}`);
 });
